ef930e503abd1049a45a74c066ccd0b60030d735
Risheng1128 <hi4u29ck02@gmial.com>
2022-03-20 18:49:44 +0800

    Add details of the file

--- eliminate.py
+++ eliminate.py
@@ -1,22 +1,25 @@
+
 import subprocess
 import numpy as np
 
 def process(datas, samples, threshold = 2):
     datas = np.array(datas)
     res = np.zeros((samples, 3))
-    
+    # 分別計算 kernel user 及 kernel to user 的平均
     mean = [datas[:,0].mean(), datas[:,1].mean(), datas[:,2].mean()]
+    # 分別計算 kernel user 及 kernel to user 的標準差
     std = [datas[:,0].std(), datas[:,1].std(), datas[:,2].std()]
     cnt = 0 # 計算有幾組資料被捨去
     for i in range(samples):
         for j in range(runs):
-            tmp = np.abs((datas[j * samples + i] - mean) / std)
-            if tmp[0] > threshold or tmp[1] > threshold or tmp[2] > threshold: # 有一個資料過大，整組捨去
+            tmp = np.abs((datas[j * samples + i] - mean) / std) # 計算資料是多少標準差
+            # 如果某一組的某個資料過大，整組捨去
+            if tmp[0] > threshold or tmp[1] > threshold or tmp[2] > threshold:
                 cnt += 1
                 datas[j * samples + i] = [0, 0, 0]
             res[i] += datas[j * samples + i]
-        res[i] /= (runs - cnt)
-        cnt = 0
+        res[i] /= (runs - cnt) # 剩下的資料取平均
+        cnt = 0 # count 歸 0
     return res
 
 if __name__ == '__main__':
@@ -25,11 +28,14 @@ if __name__ == '__main__':
     datas = []
 
     for i in range(runs):
-        subprocess.run('sudo taskset 0x1 ./client > out', shell = True)
+        # 執行採樣
+        subprocess.run('sudo ./client > out', shell = True)
+        # 讀取資料
         fr = open('out', 'r')
         for line in fr.readlines():
             datas.append(line.split(' ', 2))
         fr.close()
     
     datas = np.array(datas).astype(np.double)
+    # 存回資料
     np.savetxt('final', process(datas, samples))
