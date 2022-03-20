2f9ab08d390ec04db0893d486c9f93e34c3c879a
Risheng1128 <hi4u29ck02@gmial.com>
2022-03-20 15:22:41 +0800

    Measure the executing time of kernel and user space

--- client.c
+++ client.c
@@ -3,14 +3,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
 
 #define FIB_DEV "/dev/fibonacci"
 
+long long elapse(struct timespec *start, struct timespec *end)
+{
+    return (long long) (end->tv_sec - start->tv_sec) * 1e9 +
+           (long long) (end->tv_nsec - start->tv_nsec);
+}
+
 int main()
 {
     char buf[256];
-    int offset = 500; /* TODO: try test something bigger than the limit */
+    int offset = 100; /* TODO: try test something bigger than the limit */
 
     int fd = open(FIB_DEV, O_RDWR);
     if (fd < 0) {
@@ -18,10 +25,15 @@ int main()
         exit(1);
     }
 
-    for (int i = 0; i <= offset; i++) {
+    struct timespec start, end;
+    for (int i = 0; i < offset; i++) {
         lseek(fd, i, SEEK_SET);
+        clock_gettime(CLOCK_MONOTONIC, &start);
         read(fd, buf, 256);
-        printf("%d %s\n", i, buf);
+        clock_gettime(CLOCK_MONOTONIC, &end);
+        long int ktime = write(fd, buf, 256);
+        printf("%lld %ld %lld\n", elapse(&start, &end), ktime,
+               elapse(&start, &end) - ktime);
     }
 
     close(fd);
